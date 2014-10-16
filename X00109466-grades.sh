b3f161692f1b573b6eb159ecaf6d3c1b07506fd3
x00109466 <x00109466@gmail.com>
2014-10-16 10:55:06 +0100

    Misha

---
+++ X00109466-grades.sh
@@ -0,0 +1,27 @@
+echo Inputs: 60 + 50
+echo Expected Result: Pass
+java grades 60 50
+
+echo Inputs: 41 + 70
+echo Expected Result: Pass
+java grades 41 70 
+
+echo Inputs: 12 + 60
+echo Expected Result: Component Fail
+java grades 12 60
+
+echo Inputs: 69 + 69
+echo Expected Result: Pass
+java grades 69 69
+
+echo Inputs: 32 + 99
+echo Expected Result: Component Fail
+java grades 32 99
+
+echo Inputs: 61 + 78
+echo Expected Result: Pass
+java grades 61 78
+
+echo Inputs: 110 + 78
+echo Expected Result: Invalid Input
+java grades 110 78
